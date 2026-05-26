1ad7a4f5198678345c5511d9f4e3f6718899e416
Allan <allangomes_07@outlook.com>
2026-05-26 11:57:16 -0300

    desenvolvimento login

---
+++ js/login.js
@@ -0,0 +1,17 @@
+
+const campo = document.getElementById("emaillogin");
+const senha = document.getElementById("senhalogin")
+const btn = document.getElementById("nitidezentrar");
+const cadastro = document.getElementById("nitidezentrar1");
+
+
+senha.addEventListener("input", ()=> {
+    if (senha.value.trim() !== ""){
+        btn.disabled =false;
+        cadastro.disabled=false;
+    }
+    else{
+        btn.disabled=true;
+        cadastro.disabled=true;s
+    }
+});
