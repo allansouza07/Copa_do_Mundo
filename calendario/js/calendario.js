8418b32e67e965cf2d5f5ef9a9efc8bf6649c878
Allan <allangomes_07@outlook.com>
2026-06-17 00:03:27 -0300

    aba de calendário

---
+++ calendario/js/calendario.js
@@ -0,0 +1,169 @@
+function verRodada(nomeRodada) {
+    
+var painel = document.getElementById("secao-jogos");
+
+//--------1 RODADA
+
+if (nomeRodada == 'r1') {
+        
+        painel.innerHTML = `
+            <h2>--- 1ª RODADA ---</h2>
+
+            <p><b>11 de junho de 2026 (Quinta-feira)</b></p>
+            <p>16h00 – Grupo A: México x África do Sul</p>
+            <p>23h00 – Grupo A: República da Coreia x República Tcheca</p>
+            <hr>
+            <p><b>12 de junho de 2026 (Sexta-feira)</b></p>
+            <p>16h00 – Grupo B: Canadá x Bósnia e Herzegovina</p>
+            <p>22h00 – Grupo D: Estados Unidos x Paraguai</p>
+            <hr>
+            <p><b>13 de junho de 2026 (Sábado)</b></p>
+            <p>16h00 – Grupo B: Catar x Suíça</p>
+            <p>19h00 – Grupo C: Brasil x Marrocos</p>
+            <p>22h00 – Grupo C: Haiti x Escócia</p>
+            <hr>
+            <p><b>14 de junho de 2026 (Domingo)</b></p>
+            <p>01h00 – Grupo D: Austrália x Turquia</p>
+            <p>14h00 – Grupo E: Alemanha x Curaçau</p>
+            <p>17h00 – Grupo F: Holanda x Japão</p>
+            <p>20h00 – Grupo E: Costa do Marfim x Equador</p>
+            <p>23h00 – Grupo F: Suécia x Tunísia</p>
+            <hr>
+            <p><b>15 de junho de 2026 (Segunda-feira)</b></p>
+            <p>13h00 – Grupo H: Espanha x Cabo Verde</p>
+            <p>16h00 – Grupo G: Bélgica x Egito</p>
+            <p>19h00 – Grupo H: Arábia Saudita x Uruguai</p>
+            <p>22h00 – Grupo G: Irã x Nova Zelândia</p>
+            <hr>
+            <p><b>16 de junho de 2026 (Terça-feira)</b></p>
+            <p>16h00 – Grupo I: França x Senegal</p>
+            <p>19h00 – Grupo I: Iraque x Noruega</p>
+            <p>22h00 – Grupo J: Argentina x Argélia</p>
+            <hr>
+            <p><b>17 de junho de 2026 (Quarta-feira)</b></p>
+            <p>01h00 – Grupo J: Áustria x Jordânia</p>
+            <p>14h00 – Grupo K: Portugal x República Democrática do Congo</p>
+            <p>17h00 – Grupo L: Inglaterra x Croácia</p>
+            <p>20h00 – Grupo L: Gana x Panamá</p>
+            <p>21h00 – Grupo K: Uzbequistão x Colômbia</p>
+        `;
+                       } 
+
+//-------------- 2 RODADA
+
+else if (nomeRodada == 'r2') {
+        
+        painel.innerHTML = `
+            <h2>--- 2ª RODADA ---</h2>
+
+            <p><b>18 de junho de 2026 (Quinta-feira)</b></p>
+            <p>13h00 – Grupo A: República Tcheca x África do Sul</p>
+            <p>16h00 – Grupo B: Suíça x Bósnia e Herzegovina</p>
+            <p>19h00 – Grupo B: Canadá x Catar</p>
+            <p>22h00 – Grupo A: México x República da Coreia</p>
+            <hr>
+            <p><b>19 de junho de 2026 (Sexta-feira)</b></p>
+            <p>16h00 – Grupo D: Estados Unidos x Austrália</p>
+            <p>19h00 – Grupo C: Escócia x Marrocos</p>
+            <p>21h30 – Grupo C: Brasil x Haiti</p>
+            <hr>
+            <p><b>20 de junho de 2026 (Sábado)</b></p>
+            <p>00h00 – Grupo D: Turquia x Paraguai</p>
+            <p>14h00 – Grupo F: Holanda x Suécia</p>
+            <p>17h00 – Grupo E: Alemanha x Costa do Marfim</p>
+            <p>21h00 – Grupo E: Equador x Curaçau</p>
+            <p>23h00 – Grupo F: Tunísia x Japão</p>
+            <hr>
+            <p><b>21 de junho de 2026 (Domingo)</b></p>
+            <p>13h00 – Grupo H: Espanha x Arábia Saudita</p>
+            <p>16h00 – Grupo G: Bélgica x Irã</p>
+            <p>19h00 – Grupo H: Uruguai x Cabo Verde</p>
+            <p>22h00 – Grupo G: Nova Zelândia x Egito</p>
+            <hr>
+            <p><b>22 de junho de 2026 (Segunda-feira)</b></p>
+            <p>14h00 – Grupo J: Argentina x Áustria</p>
+            <p>18h00 – Grupo I: França x Iraque</p>
+            <p>21h00 – Grupo I: Noruega x Senegal</p>
+            <hr>
+            <p><b>23 de junho de 2026 (Terça-feira)</b></p>
+            <p>00h00 – Grupo J: Jordânia x Argélia</p>
+            <p>14h00 – Grupo K: Portugal x Uzbequistão</p>
+            <p>17h00 – Grupo L: Inglaterra x Gana</p>
+            <p>20h00 – Grupo L: Panamá x Croácia</p>
+            <p>23h00 – Grupo K: Colômbia x República Democrática do Congo</p>
+        `;
+                            } 
+
+//-------------3 RODADA
+
+    else if (nomeRodada == 'r3') {
+        
+
+        painel.innerHTML = `
+            <h2>--- 3ª RODADA ---</h2>
+
+            <p><b>24 de junho de 2026 (Quarta-feira)</b></p>
+            <p>16h00 – Grupo B: Suíça x Canadá</p>
+            <p>16h00 – Grupo B: Bósnia e Herzegovina x Catar</p>
+            <p>19h00 – Grupo C: Escócia x Brasil</p>
+            <p>19h00 – Grupo C: Marrocos x Haiti</p>
+            <p>22h00 – Grupo A: República Tcheca x México</p>
+            <p>22h00 – Grupo A: África do Sul x República da Coreia</p>
+            <hr>
+            <p><b>25 de junho de 2026 (Quinta-feira)</b></p>
+            <p>17h00 – Grupo E: Equador x Alemanha</p>
+            <p>17h00 – Grupo E: Curaçau x Costa do Marfim</p>
+            <p>20h00 – Grupo F: Japão x Suécia</p>
+            <p>20h00 – Grupo F: Tunísia x Holanda</p>
+            <p>23h00 – Grupo D: Turquia x Estados Unidos</p>
+            <p>23h00 – Grupo D: Paraguai x Austrália</p>
+            <hr>
+            <p><b>26 de junho de 2026 (Sexta-feira)</b></p>
+            <p>16h00 – Grupo I: Noruega x França</p>
+            <p>16h00 – Grupo I: Senegal x Iraque</p>
+            <p>21h00 – Grupo H: Cabo Verde x Arábia Saudita</p>
+            <p>21h00 – Grupo H: Uruguai x Espanha</p>
+            <hr>
+            <p><b>27 de junho de 2026 (Sábado)</b></p>
+            <p>00h00 – Grupo G: Egito x Irã</p>
+            <p>00h00 – Grupo G: Nova Zelândia x Bélgica</p>
+            <p>18h00 – Grupo L: Panamá x Inglaterra</p>
+            <p>18h00 – Grupo L: Croácia x Gana</p>
+            <p>20h30 – Grupo K: Colômbia x Portugal</p>
+            <p>20h30 – Grupo K: República Democrática do Congo x Uzbequistão</p>
+            <p>23h00 – Grupo J: Argélia x Áustria</p>
+            <p>23h00 – Grupo J: Jordânia x Argentina</p>
+        `;
+                             } 
+
+
+//---------------------MATA-MATA
+
+    else if (nomeRodada == 'mata') {
+        
+        painel.innerHTML = `
+            <h2>--- FASES DE MATA-MATA ---</h2>
+
+            <p><b>Fase de 32-avos</b></p>
+            <p>28 de junho (Domingo) ao dia 03 de julho (Sexta-feira)</p>
+            <hr>
+            <p><b>Oitavas de Final</b></p>
+            <p>04 de julho (Sábado) ao dia 07 de julho (Terça-feira)</p>
+            <hr>
+            <p><b>Quartas de Final</b></p>
+            <p>09 de julho (Quinta-feira) ao dia 12 de julho (Sábado)</p>
+            <hr>
+            <p><b>Semifinais</b></p>
+            <p>14 de julho (Terça-feira) e 15 de julho (Quarta-feira)</p>
+            <hr>
+            <p><b>Disputa de 3º Lugar</b></p>
+            <p>18 de julho (Sábado)</p>
+            <hr>
+            <p><b>🏆 Grande Final 🏆</b></p>
+            <p>19 de julho de 2026 (Domingo) - Nova York / Nova Jersey</p>
+        `;
+                                    }
+}
+
+//INICIAR RODADA 1
+verRodada('r1');
